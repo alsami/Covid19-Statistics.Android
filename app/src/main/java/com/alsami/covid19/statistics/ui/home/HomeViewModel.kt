2e81dfaae18b54c3473d47bf06835f80822c8780
alsami <al.khatib.sami1990@gmail.com>
2020-05-17 08:33:31 +0200

    chore: add project

---
+++ app/src/main/java/com/alsami/covid19/statistics/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.alsami.covid19.statistics.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
